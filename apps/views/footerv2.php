fd7225fd68de4fa0194987f096f62cd0834aa387
Martinus Alexander <martinus.alexander@advance.ai>
2019-08-23 10:38:58 +0800

    Add Whatsapp floating button

--- apps/views/footerv2.php
+++ apps/views/footerv2.php
@@ -1,4 +1,4 @@
-    
+
     <footer id="footer" class="midnight-blue">
         <div class="container">
             <div class="row">
@@ -23,5 +23,31 @@
     <script src="<?=FILEPATH_UI;?>js/jquery.isotope.min.js"></script>
     <script src="<?=FILEPATH_UI;?>js/main.js"></script>
     <script src="<?=FILEPATH_UI;?>js/wow.min.js"></script>
+
+    <!-- Whatsapp -->
+    <style>
+    .float{
+    	position:fixed;
+    	width:60px;
+    	height:60px;
+    	bottom:40px;
+    	right:40px;
+    	background-color:#25d366;
+    	color:#FFF;
+    	border-radius:50px;
+    	text-align:center;
+        font-size:30px;
+    	box-shadow: 2px 2px 3px #999;
+        z-index:100;
+    }
+
+    .my-float{
+    	margin-top:16px;
+    }
+    </style>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <a href="https://api.whatsapp.com/send?phone=6585917218" class="float" target="_blank">
+        <i class="fa fa-whatsapp my-float"></i>
+    </a>
 </body>
 </html>
